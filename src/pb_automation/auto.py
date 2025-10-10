6a1f2c95031ad9411717b9cfd5618388713b2437
Saif Hassan <msaifhassan@gmail.com>
2025-10-09 21:40:00 -0500

    Added logic to search through preferred times when clicking on reservation

--- src/pb_automation/auto.py
+++ src/pb_automation/auto.py
@@ -40,8 +40,34 @@ def verify_successful(page) -> False:
         print("Reservation failed: Confirm button not found. Either you have already booked or no slots available.")
         return False
     
+    
+def pick_time_slot(page) -> str:
+    """Default reservation time is 6:00pm for weekdays and 8:00am for weekends.
+       For weekday, looks links with text matching 6:00pm or 6:30pm or 7:00pm etc and returns the first match. 
+    """
+    
+    weekday_preferred_times = [ "6:00pm", "6:30pm", "7:00pm", "7:30pm", "8:00pm" ]
+    weekend_preferred_times = [ "8:00am", "8:30am", "9:00am", "9:30am", "10:00am" ]
+    
+    # logic to determine if today is a weekday or weekend
+    today = datetime.today()
+    if today.weekday() < 5:  # Monday to Friday are considered weekdays
+        result = get_slot(page, weekday_preferred_times)
+    else:  # Saturday and Sunday are considered weekends
+        result = get_slot(page, weekend_preferred_times)
+    return result
+    
+def get_slot(page, preferred_times) -> bool:
+    for time in preferred_times:
+        try:
+            page.get_by_role("link", name=time).click()
+            print(f"Successfully clicked on {time} time slot.")
+            return True
+        except:
+            continue
+    return False
 
-def run(page, reservation_date: str, duration: str = "120", reservation_time: str = "6:00pm") -> None:
+def run(page, reservation_date: str, duration: str = "120") -> None:
     page.get_by_role("link", name="Reserve a Court").click()
     page.locator("a").filter(has_text="Racquetball").click()
     page.locator("#component_chosen").get_by_text("Tennis").click()
@@ -58,11 +84,13 @@ def run(page, reservation_date: str, duration: str = "120", reservation_time: st
     page.get_by_text(duration).click()
     
     page.get_by_role("button", name="Search").click()
-    page.get_by_role("link", name=reservation_time).click()
+    got_res = pick_time_slot(page)
+    
+    if not got_res:
+        print("No preferred time slots available.")
+        return False
     
     return verify_successful(page)
-    
-
     
 
 def main():
@@ -70,7 +98,8 @@ def main():
         reservation_date = get_date()
         print(f"Reserving court for date: {reservation_date}")
         page, context, browser = login(playwright, os.getenv("PB_USERNAME"), os.getenv("PB_PASSWORD"))
-        result = run(page, reservation_date, duration="120 Min", reservation_time="6:00pm")
+        
+        result = run(page, reservation_date, duration="120 Min")
         
         print(f"Result: {result}")
         
